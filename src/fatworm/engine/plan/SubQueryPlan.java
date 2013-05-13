f234117169010a2989044c58473edf1ced35eda0
joey2005 <joey2005.yali@gmail.com>
2013-05-14 07:45:49 +0800

    commit at 2013/5/14 7:46

---
+++ src/fatworm/engine/plan/SubQueryPlan.java
@@ -0,0 +1,60 @@
+package fatworm.engine.plan;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import fatworm.indexing.scan.Scan;
+import fatworm.indexing.schema.Schema;
+import fatworm.indexing.table.Record;
+
+public class SubQueryPlan extends Plan {
+	
+	private Plan subPlan;
+	private Schema schema;
+	private Record record;
+	private Scan scan;
+	private int planID;
+	
+	public SubQueryPlan(Plan subPlan) {
+		this.subPlan = subPlan;
+		schema = subPlan.getSchema();
+		record = null;
+		scan = null;
+	}
+	
+	public void remove() {
+		if (scan != null) {
+			scan.close();
+		}
+	}
+
+	@Override
+	public int getPlanID() {
+		return planID;
+	}
+
+	@Override
+	public Scan createScan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Plan subPlan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Schema getSchema() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		String result = subPlan.toString() + "\n";
+		result += "Plan #" + (planID = Plan.planCount++) + "<-" + "(SubQueryPlan)" + subPlan.getPlanID();
+		return result;
+	}
+}
